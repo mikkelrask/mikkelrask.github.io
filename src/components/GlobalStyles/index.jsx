f5166658e9af9a04525358670e5b6267c6a7f551
Mikkel Rask <mikkelrask@pm.me>
2024-06-26 23:40:35 +0200

    chore: Update font styles and global styles

--- src/components/GlobalStyles/index.jsx
+++ src/components/GlobalStyles/index.jsx
@@ -1,14 +1,34 @@
-import { createGlobalStyle } from "styled-components"
-import reset from "styled-reset"
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
 
-  body {
-    font-family: 'Noto Sans KR', sans-serif;
-    background: ${props => props.theme.colors.bodyBackground};
+  @font-face {
+    font-family: 'HackNFM';
+    src: 
+      url('../../fonts/HackNFM-Regular.woff') format('woff'),
+      url('../../fonts/HackNFM-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
   }
 
-`
+  @font-face {
+    font-family: 'FantasqueSansMNF';
+    src: 
+      url('../../fonts/FantasqueSansMNFM-Regular.woff') format('woff'),
+      url('../../fonts/FantasqueSansMNFM-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
 
-export default GlobalStyles
+  body {
+    font-family: 'HackNFM', sans-serif;
+    background: ${props => props.theme.colors.bodyBackground};
+  };
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'FantasqueSansMNF', sans-serif;
+  };
+`;
+
+export default GlobalStyles;
