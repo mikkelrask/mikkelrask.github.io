81bf7a97b8450006ff50ac55996c738786c516b2
Mikkel Rask <mikkelrask@pm.me>
2024-07-05 10:29:09 +0200

    more meta stuff

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -7,8 +7,14 @@ const SEO = ({ title, description, url, image }) => {
     <Helmet>
       <title>{title}</title>
       <meta property="og:url" content={url} />
+      <meta property="og:author" content="@mikkelrask" />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="da_DK" />
+      <meta name="twitter:card" content="summary_large_image" />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
